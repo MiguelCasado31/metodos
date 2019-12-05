656f915c0d279807263c27d400bcd8d92c9fd1e0
casadomiguel31@hotmail.com <casadomiguel31@hotmail.com>
2019-12-05 10:36:24 +0100

    Creada la variable constante de un número

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -2,10 +2,17 @@ package ejercicios;
 
 public class Metodos {
 
-	public static String String (String varUNo, String varDos) {
+	public static String String  (String varUNo, String varDos) {
 		String varUno = "adiós";
 		String varDOs = "Hasta luego Mari Carmen";
 		String varTres= varUno +" "+ varDos;
 	 return  varTres;
 	 }
+	public static void numero() {
+	char a=77;
+	System.out.println(a);
+	}
+		
+
 }
+
