1514d03ba427686eb434cb1ea83538a1b495a34e
casadomiguel31@hotmail.com <casadomiguel31@hotmail.com>
2019-12-05 10:19:10 +0100

    modificando la clase metodos

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -2,4 +2,10 @@ package ejercicios;
 
 public class Metodos {
 
+	public static String String (String varUNo, String varDos) {
+		String varUno = "adiós";
+		String varDOs = "Hasta luego Mari Carmen";
+		String varTres= varUno +" "+ varDos;
+	 return  varTres;
+	 }
 }
